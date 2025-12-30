d28e8f1cbdd6bbce9bd7658230f026ec30b8ed1a
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-12-30 18:01:03 +0100

    feat: setup tabs navigator for both screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+const TabsLayout = () => {
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: 'white',
+				tabBarInactiveTintColor: 'purple',
+				tabBarStyle: {
+					backgroundColor: '#1e293b',
+					borderTopWidth: 1,
+					borderTopColor: 'purple',
+					height: 90,
+					paddingTop: 10,
+					paddingBottom: 30,
+				},
+				tabBarLabelStyle: {
+					fontSize: 12,
+					fontWeight: 600,
+				},
+				headerShown: false,
+			}}>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Todos',
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name='flash-outline' size={size} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='settings'
+				options={{
+					title: 'Settings',
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name='settings' size={size} color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
+};
+
+export default TabsLayout;
