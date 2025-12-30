09821a578f62554caaf9b99d223f46348aeba16d
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-12-30 18:16:01 +0100

    feat: implement custom hook for toggling light/dark mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,19 @@
+import { useTheme } from '@/hooks/useTheme';
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 const TabsLayout = () => {
+	const { colors } = useTheme();
+
 	return (
 		<Tabs
 			screenOptions={{
-				tabBarActiveTintColor: 'white',
-				tabBarInactiveTintColor: 'purple',
+				tabBarActiveTintColor: colors.primary,
+				tabBarInactiveTintColor: colors.textMuted,
 				tabBarStyle: {
-					backgroundColor: '#1e293b',
+					backgroundColor: colors.surface,
 					borderTopWidth: 1,
-					borderTopColor: 'purple',
+					borderTopColor: colors.border,
 					height: 90,
 					paddingTop: 10,
 					paddingBottom: 30,
