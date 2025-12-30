b9fa50b0b6d24b460749f8b20709eaee86b6ac25
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-12-30 23:24:56 +0100

    feat: install expo linear gradient, add home styles and update home with header component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
-import { Text, View } from 'react-native';
+import { createHomeStyles } from '@/assets/styles/home.styles';
+import { useTheme } from '@/hooks/useTheme';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Header from '@/components/Header';
 
 export default function Index() {
+	const { colors } = useTheme();
+
+	const styles = createHomeStyles(colors);
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-			<Text>Hello world</Text>
-		</View>
+		<LinearGradient colors={colors.gradients.background} style={styles.container}>
+			<StatusBar barStyle={colors.statusBarStyle} />
+			<SafeAreaView style={styles.safeArea}>
+				<Header />
+			</SafeAreaView>
+		</LinearGradient>
 	);
 }
