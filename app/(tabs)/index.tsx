15067f8cb248c32ca2883b35df9ce83e0fb49e62
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-12-30 18:47:26 +0100

    feat: install and setup convex db for managing todos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello world</Text>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<Text>Hello world</Text>
+		</View>
+	);
 }
